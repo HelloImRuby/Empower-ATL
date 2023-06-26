9b874201c8fc0fa5f23b99171423bab027375a0e
Ruby Darku <rdarku147@gmail.com>
2023-06-26 15:02:44 -0400

    Initial Commit

---
+++ Empower ATL/Empower_ATLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Empower_ATLApp.swift
+//  Empower ATL
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Empower_ATLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
